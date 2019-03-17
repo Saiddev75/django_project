c45009ec7f6fc04b4eb0485941dd64cfbee7a6c5
Saïd Belhadj <saidbelhadj75019@gmail.com>
2019-03-17 13:26:01 +0100

    Ajout d'un thème bootstrap que j'ai customiser dans un fichier base.html afin que le code ne se répète pas et creation d'un static  folder qui reunira tous les fichiers static tel css et js (main.css)

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
+
+posts = [
+    {
+        'authors': 'Saïd Belhadj',
+        'title': 'Le Blog de Saïd Belhadj',
+        'content': 'Bienvenue sur mon Blog',
+        'date_posted': '24/01/2019 '
+    },
+    {
+        'authors': 'Adam Belhadj',
+        'title': 'Le Blog de Adam Belhadj',
+        'content': 'Bienvenue sur mon Blog',
+        'date_posted': '24/01/2019 '
+    },
+    {
+        'authors': 'Adel Belhadj',
+        'title': 'Le Blog de Adel Belhadj',
+        'content': 'Bienvenue sur mon Blog',
+        'date_posted': '24/01/2019 '
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'about'})
